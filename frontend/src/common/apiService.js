c9b967e728533a220c3f17c250a1284c9b940d18
Taras Katrych <KatrychTaras@gmail.com>
2020-01-13 14:50:23 +0100

    add page not found view

---
+++ frontend/src/common/apiService.js
@@ -0,0 +1,29 @@
+import {CSRF_TOKEN} from "./csrfToken";
+
+function getJson(response) {
+    if (response.status === 204){
+        return "";
+    }else if (response.status === 404){
+        return null;
+    }else {
+        return response.json()
+    }
+}
+
+
+
+function apiService(endpoint, method, data) {
+    const config = {
+        method: method || "GET",
+        body: data !== undefined ? JSON.stringify(data) : null,
+        headers:{
+            "content-type": "application/json",
+            'X-CSRFTOKEN': CSRF_TOKEN
+        }
+    };
+    return fetch(endpoint, config)
+        .then(getJson)
+        .catch(err => console.log(err))
+}
+
+export { apiService }
