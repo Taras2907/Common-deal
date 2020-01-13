b0fab42838320ebff7b2ee05a85522af953ad7c0
Taras Katrych <KatrychTaras@gmail.com>
2020-01-13 19:01:11 +0100

    configure sendgrid email send

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
+from rest_auth import views
 
-
-urlpatterns= [
+urlpatterns = [
 
     path('api-auth/', include('rest_framework.urls')),
 
     path('rest-auth/', include('rest_auth.urls')),
 
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    path('rest-auth/password/reset/confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
+from django.core.mail import send_mail
 from rest_framework.response import Response
 from rest_framework import status
+from django.utils.translation import ugettext_lazy as _
 from rest_framework.generics import get_object_or_404
-
-
-
+from rest_auth.views import PasswordResetView
+from django.contrib.sites.models import Site
 
 
 
