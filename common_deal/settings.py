b436a5e0e84b5fa3369fe9abd2180a4f0d926354
Taras2907 <katrychtaras@gmail.com>
2020-02-26 15:57:45 +0100

    change azure location

--- common_deal/settings.py
+++ common_deal/settings.py
@@ -183,8 +183,8 @@ WEBPACK_LOADER = {
 AZURE_ACCOUNT_NAME = 'frontendcommondeal'
 AZURE_ACCOUNT_KEY = 'Am6+AvjIlMNUfJQL/6u8zlykD591OxOKbssUzIQ3OD6HvYh6cvxtU0SbTUxR4CAWo60+klitxxfbvGFi/ls1CQ=='
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
-AZURE_LOCATION = "static"
-AZURE_CONTAINER = "static"
+AZURE_LOCATION = "$web"
+AZURE_CONTAINER = "$web"
 
 STATIC_LOCATION = 'static'
 STATIC_URL = f'https://'
