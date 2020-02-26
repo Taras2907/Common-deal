dd21a05ea9cef5f2682d47e53a74d9ecb02eec2c
Taras2907 <katrychtaras@gmail.com>
2020-02-26 14:00:02 +0100

    change basedir

--- common_deal/settings.py
+++ common_deal/settings.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = "/wwwroot/"
+BASE_DIR = "https://common-deal.scm.azurewebsites.net//wwwroot/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
