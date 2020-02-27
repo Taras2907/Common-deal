908f2c67bcc00dfb0e39f1173d2f419177afab15
Taras2907 <katrychtaras@gmail.com>
2020-02-27 20:32:49 +0100

    fix bugs

--- frontend/src/views/Category.vue
+++ frontend/src/views/Category.vue
@@ -2,7 +2,7 @@
     <v-container>
         <Sorting @sort="sortProducts"></Sorting>
         <v-row>
-            <v-col md="3" sm="2" v-for="(product, index) in products" :key="index">
+            <v-col md="3" sm="12" v-for="(product, index) in products" :key="index">
                 <router-link :to="{name: 'product-detail', params:{id: product.id}}">
                     <Product :product="product"/>
                 </router-link>
